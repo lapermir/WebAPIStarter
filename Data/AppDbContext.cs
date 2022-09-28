81268108821606f1495e93d5d31b15684ba7afae
abbasymot <motameditalemi@gmail.com>
2022-09-28 16:58:02 +0330

    Seed Fake Sample Data

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,18 +5,24 @@ namespace WebAPIStarter.Data;
 
 public class AppDbContext : DbContext
 {
+    private readonly List<Sample> _data = new ()
+    {
+        new Sample { Id = 1, Title = "Amirabbas" },
+        new Sample { Id = 2, Title = "IRAN" },
+        new Sample { Id = 3, Title = "Sample N03" },
+    };
+
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base (options)
     {
         
     }
 
-    // TODO
-    // Seed Fake Data...
-    // protected override void OnModelCreating()
-    // {
-
-    // }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        // Seed Fake Data...
+        builder.Entity<Sample>().HasData(_data);
+    }
 
     public DbSet<Sample> Samples { get; set; }
 }
